e6c9af618495e9415cb648140ebae0121f84cd56
Lavender <hi@lavylavender.com>
2025-09-17 02:35:46 -0700

    OBS

--- src/configuration.nix
+++ src/configuration.nix
@@ -34,6 +34,26 @@
 
     services.flatpak.enable = true;
 
+programs.obs-studio = {
+    enable = true;
+
+    # optional Nvidia hardware acceleration
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi #optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
     programs.steam.enable = true;
     programs.steam.gamescopeSession.enable = true;
     programs.gamemode.enable = true;
