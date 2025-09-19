7ce4b3c0e518bd6134f01b7e57f41a23624fc104
Lavender <hi@lavylavender.com>
2025-09-18 19:50:59 -0700

    thunderbirddd

--- src/programs/thunderbird.nix
+++ src/programs/thunderbird.nix
@@ -6,10 +6,9 @@
   ...
 }: {
   config.programs.thunderbird = {
-    inherit (config.var) username;
     # https://home-manager-options.extranix.com/?query=programs.thunderbird.&release=master
     enable = true;
-    profiles.${config.var.username} = {
+    profiles.lavender = {
       isDefault = true;
     };
   };
